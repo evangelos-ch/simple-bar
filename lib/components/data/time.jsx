86d0b9cbaee3d6667d85b85609d1deaebb5afa2e
evangelos-ch <angelos.h@hotmail.com>
2023-07-14 11:08:18 +0100

    Point time widget and weather widget to MacOS apps

--- lib/components/data/time.jsx
+++ lib/components/data/time.jsx
@@ -11,70 +11,52 @@ export { timeStyles as styles } from "../../styles/components/data/time";
 const settings = Settings.get();
 const { widgets, timeWidgetOptions } = settings;
 const { timeWidget } = widgets;
-const { refreshFrequency, hour12, dayProgress, showSeconds } =
-  timeWidgetOptions;
+const { refreshFrequency, hour12, dayProgress, showSeconds } = timeWidgetOptions;
 
 const DEFAULT_REFRESH_FREQUENCY = 1000;
-const REFRESH_FREQUENCY = Settings.getRefreshFrequency(
-  refreshFrequency,
-  DEFAULT_REFRESH_FREQUENCY
-);
+const REFRESH_FREQUENCY = Settings.getRefreshFrequency(refreshFrequency, DEFAULT_REFRESH_FREQUENCY);
 
-const displayNotificationCenter = () =>
-  Uebersicht.run(
-    `osascript -e 'tell application "System Events" to click menu bar item "Clock" of menu bar 1 of application process "ControlCenter"'`
-  );
+const openClock = (e) => {
+    Utils.clickEffect(e);
+    const appName = "Clock";
+    Uebersicht.run(`open -a "${appName}"`);
+};
 
 export const Widget = () => {
-  const [state, setState] = Uebersicht.React.useState();
-  const [loading, setLoading] = Uebersicht.React.useState(timeWidget);
+    const [state, setState] = Uebersicht.React.useState();
+    const [loading, setLoading] = Uebersicht.React.useState(timeWidget);
 
-  const options = {
-    hour: "numeric",
-    minute: "numeric",
-    second: showSeconds ? "numeric" : undefined,
-    hour12,
-  };
+    const options = {
+        hour: "numeric",
+        minute: "numeric",
+        second: showSeconds ? "numeric" : undefined,
+        hour12,
+    };
 
-  const getTime = () => {
-    const time = new Date().toLocaleString("en-UK", options);
-    setState({ time });
-    setLoading(false);
-  };
+    const getTime = () => {
+        const time = new Date().toLocaleString("en-UK", options);
+        setState({ time });
+        setLoading(false);
+    };
 
-  useWidgetRefresh(timeWidget, getTime, REFRESH_FREQUENCY);
+    useWidgetRefresh(timeWidget, getTime, REFRESH_FREQUENCY);
 
-  if (loading) return <DataWidgetLoader.Widget className="time" />;
-  if (!state) return null;
-  const { time } = state;
+    if (loading) return <DataWidgetLoader.Widget className="time" />;
+    if (!state) return null;
+    const { time } = state;
 
-  const [dayStart, dayEnd] = [new Date(), new Date()];
-  dayStart.setHours(0, 0, 0);
-  dayEnd.setHours(0, 0, 0);
-  dayEnd.setDate(dayEnd.getDate() + 1);
-  const range = dayEnd - dayStart;
-  const diff = Math.max(0, dayEnd - new Date());
-  const fillerWidth = (100 - (100 * diff) / range) / 100;
+    const [dayStart, dayEnd] = [new Date(), new Date()];
+    dayStart.setHours(0, 0, 0);
+    dayEnd.setHours(0, 0, 0);
+    dayEnd.setDate(dayEnd.getDate() + 1);
+    const range = dayEnd - dayStart;
+    const diff = Math.max(0, dayEnd - new Date());
+    const fillerWidth = (100 - (100 * diff) / range) / 100;
 
-  const onClick = (e) => {
-    Utils.clickEffect(e);
-    displayNotificationCenter();
-  };
-
-  return (
-    <DataWidget.Widget
-      classes="time"
-      Icon={Icons.Clock}
-      onClick={onClick}
-      disableSlider
-    >
-      {time}
-      {dayProgress && (
-        <div
-          className="time__filler"
-          style={{ transform: `scaleX(${fillerWidth})` }}
-        />
-      )}
-    </DataWidget.Widget>
-  );
+    return (
+        <DataWidget.Widget classes="time" Icon={Icons.Clock} onClick={openClock} disableSlider>
+            {time}
+            {dayProgress && <div className="time__filler" style={{ transform: `scaleX(${fillerWidth})` }} />}
+        </DataWidget.Widget>
+    );
 };
